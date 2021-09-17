2b1bdacc0c45a7ab2e6a5cecf6ab253a1ddd213e
Pawan Gupta <guptapawanro2017@gmail.com>
2021-09-17 17:31:17 +0530

    Print Statement Java

---
+++ BasicJava/Print.java
@@ -0,0 +1,8 @@
+package BasicJava;
+
+public class Print {
+   public static void main(String[] args) {
+       System.out.print("This is the Printing Statement");
+       System.out.print("\nHello World!!");
+   } 
+}
--- HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream> // Header file
-using namespace std;
-
-int main(int argc, char const *argv[]){   // Main Function
-
-    cout << "Hello World!!" << endl;   // Printing Statement
-    
-    return 0;
-}
